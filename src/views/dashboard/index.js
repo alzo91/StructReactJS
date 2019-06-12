ee8ca8eebe4a0f0cef15d75e1f04d489a1cd58f8
Alisson Zotarelli <alzo.zotarelli@gmail.com>
2019-06-12 16:05:54 -0300

    Inclusão de toast mesage.

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -4,21 +4,38 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import Button from '@material-ui/core/Button';
+import { ToastContainer, toast, cssTransition } from 'react-toastify';
 import Header from '../../components/Header';
 
 import * as fnLogin from '../../store/actions/fnLogin';
 import * as fnMenus from '../../store/actions/fnMenu';
 
+import 'react-toastify/dist/ReactToastify.min.css';
+import './toast.css';
+
+const Zoom = cssTransition({
+  enter: 'zoomIn',
+  exit: 'zoomOut',
+  // default to 750ms, can be omitted
+  duration: 750,
+});
+
 const Dashboard = ({ doOpenMenu }) => (
   <div>
     <Header title="Dashboard" />
+    <ToastContainer />
     <Button
       color="primary"
       onClick={() => {
-        doOpenMenu({ open: true });
+        toast('Uhuuuuu', {
+          position: 'top-center',
+          type: 'info',
+          transition: Zoom,
+          draggable: true,
+        });
       }}
     >
-      material-ui
+      Gerar ToastNotification
     </Button>
   </div>
 );
